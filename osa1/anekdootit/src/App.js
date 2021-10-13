c3eb4fadfeee44e7868a5e47ccb4258be52b014c
kiahakala <85869923+kiahakala@users.noreply.github.com>
2021-10-13 09:01:54 +0300

    Add files via upload

---
+++ osa1/anekdootit/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+
+  const randomAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+
+  const points = new Array(anecdotes.length).fill(0);
+
+  const [votes, setVotes] = useState(points);
+
+  const vote = () => {
+    const newVote = [...votes];
+    newVote[selected] += 1;
+    setVotes(newVote);
+  };
+
+  const largestNum = Math.max(...votes);
+  const idxOfLargest = votes.indexOf(largestNum);
+
+  console.log(largestNum);
+  console.log(idxOfLargest);
+
+  return (
+    <div>
+      <h1>Anecdote of the Day</h1>
+      <p>
+        {anecdotes[selected]}
+        <br></br>
+        has {votes[selected]} votes
+      </p>
+      <Button handleClick={() => vote()} text="Vote" />
+      <Button handleClick={() => randomAnecdote()} text="Next Anecdote" />
+      <h1>Anecdote with Most Votes</h1>
+      <p>
+        {anecdotes[idxOfLargest]}
+        <br></br>
+        has {largestNum} votes
+      </p>
+    </div>
+  );
+};
+
+export default App;
