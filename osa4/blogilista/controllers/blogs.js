6d549055930a4679d9bd87500ac60353cb08f960
Kia <85869923+kiahakala@users.noreply.github.com>
2022-01-19 19:11:50 +0200

    Update blogs.js

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -65,11 +65,13 @@ blogsRouter.put('/:id', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+	const blog = await Blog.findById(request.params.id)
 	const token = getTokenFrom(request)
 	const decodedToken = jwt.verify(token, process.env.SECRET)
+	const user = await User.findById(decodedToken.id)
 
-	if (token !== decodedToken) {
-		return response.status(401).json({ error: 'No permission to remove the blog!' })
+	if (blog.user.toString() !== user.id.toString()) {
+		return response.status(401).json({ error: 'No permission to remove the blog' })
 	} else {
 		await Blog.findByIdAndRemove(request.params.id)
 		response.status(204).end()
